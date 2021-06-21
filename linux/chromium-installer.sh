bc795086047a46b6c9db689b22b0ff0318d1ddab
msiejak <msiejak02@gmail.com>
2021-06-21 19:39:20 -0400

    Initial Commit

---
+++ linux/chromium-installer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo Chromium Installer/Updater
+echo downloading...
+cacheDir=$HOME/.cache/chromium-installer
+rm -r $cacheDir
+mkdir $cacheDir
+curl -L https://download-chromium.appspot.com/dl/Linux_x64?type=snapshots --output $cacheDir/chromium.zip
+echo extracting...
+sudo rm -r /opt/google/chromium
+sudo unzip $cacheDir/chromium.zip -d /opt/google/chromium
+echo running...
+/opt/google/chromium/chrome-linux/chrome
