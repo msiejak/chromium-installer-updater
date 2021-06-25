f7a58ada6f50408cb85b9da6bd2cde84f70603ce
msiejak <msiejak02@gmail.com>
2021-06-25 16:12:48 -0400

    service runs every hour

--- linux/setup.sh
+++ linux/setup.sh
@@ -2,7 +2,10 @@
 echo setting up utility...
 sudo mv ./chromium-installer.sh /usr/bin
 sudo mv ./chromium-updater.service /etc/systemd/system
+sudo mv ./chromium-updater.timer /etc/systemd/system
 sudo mv ./run-chromium /usr/bin
-sudo systemctl start chromium-updater
-sudo systemctl enable chromium-updater
+sudo systemctl start chromium-updater.service
+sudo systemctl enable chromium-updater.service
+sudo systemctl enable chromium-updater.timer
+sudo systemctl start chromium-updater.timer
 echo done. this file can be removed
