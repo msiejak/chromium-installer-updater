fdcf473b6c5a314666f0fec86bf7c2db9f40ec38
msiejak <msiejak02@gmail.com>
2021-06-25 11:25:29 -0400

    changed storage dir to /usr/bin

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo setting up utility...
-sudo mv ./chromium-installer.sh $HOME/bin
+sudo mv ./chromium-installer.sh /usr/bin
 sudo mv ./chromium-updater.service /etc/systemd/system
-sudo mv ./run-chromium $HOME/bin
+sudo mv ./run-chromium /usr/bin
 sudo systemctl start chromium-updater
 sudo systemctl enable chromium-updater
-echo done
+echo done. this file can be removed
