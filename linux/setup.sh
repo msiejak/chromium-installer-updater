a2c32f9edb107992dd60fd387922ec5293aa695f
msiejak <msiejak02@gmail.com>
2021-06-25 11:17:10 -0400

    made setup executable

--- linux/setup.sh
+++ linux/setup.sh
@@ -2,3 +2,7 @@
 echo setting up utility...
 sudo mv ./chromium-installer.sh $HOME/bin
 sudo mv ./chromium-updater.service /etc/systemd/system
+sudo mv ./run-chromium $HOME/bin
+sudo systemctl start chromium-updater
+sudo systemctl enable chromium-updater
+echo done
