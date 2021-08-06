ff01c75c92db248c937510e5b39634206b06e96c
Michael Siejak <msiejak02@gmail.com>
2021-08-05 22:04:13 -0400

    fix a possible bug with not having root access

--- linux/chromium-installer.sh
+++ linux/chromium-installer.sh
@@ -2,9 +2,9 @@
 echo Chromium Installer/Updater
 echo downloading...
 cacheDir=/var/cache/chromium-installer
-rm -r $cacheDir
-mkdir $cacheDir
-curl -L https://download-chromium.appspot.com/dl/Linux_x64?type=snapshots --output $cacheDir/chromium.zip
+sudo rm -r $cacheDir
+sudo mkdir $cacheDir
+sudo curl -L https://download-chromium.appspot.com/dl/Linux_x64?type=snapshots --output $cacheDir/chromium.zip
 echo extracting...
 sudo rm -r /opt/google/chromium
 sudo unzip $cacheDir/chromium.zip -d /opt/google/chromium
