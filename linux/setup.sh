df40e19aa975af5ca8568fc4f20f6c16ab207007
Michael Siejak <msiejak02@gmail.com>
2021-08-05 22:22:11 -0400

    add dev warning

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# WARNING!!!! (for devs only)
+# DO NOT run this file before committing any changes you made to any files in the 'linux' directory (../)
+# All files will be MOVED to their respective places and out of this directory by running this file
+
+# Disregard the above message if you are just using this script, it is intended for the developers working on the utility only
+
 echo setting up utility...
 sudo mv ./chromium-installer.sh /usr/bin
 sudo mv ./chromium-updater.service /etc/systemd/system
