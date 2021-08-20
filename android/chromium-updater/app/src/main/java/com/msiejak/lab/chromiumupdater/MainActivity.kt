msiejak/chromium-installer-updater

commit c69cfebc88d20e670a45e277824f629bae1bd973

Improve UpdateChecker

android/chromium-updater/app/src/main/java/com/msiejak/lab/chromiumupdater/MainActivity.kt
@@ -3,7 +3,11 @@ package com.msiejak.lab.chromiumupdater
 import android.app.AlarmManager
 import android.app.DownloadManager
 import android.app.PendingIntent
-import android.content.*
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.IntentFilter
 import android.net.ConnectivityManager
 import android.net.Uri
 import android.os.Bundle
@@ -13,13 +17,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import androidx.core.net.toUri
 import androidx.lifecycle.lifecycleScope
-import androidx.work.*
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.msiejak.lab.chromiumupdater.databinding.ActivityMainBinding
 import com.msiejak.lab.chromiumupdater.service.UpdateNotificationService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.io.*
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
 import java.util.concurrent.TimeUnit
 import java.util.zip.ZipEntry
 import java.util.zip.ZipInputStream
@@ -166,17 +179,20 @@ class MainActivity : AppCompatActivity() {
 
     private fun checkForUpdate() {
         binding.progressIndicator.visibility = View.VISIBLE
-        UpdateChecker().check(this, 0) { updateAvailable, lastModified, remoteVersion, resultCode ->
-            if (updateAvailable == UpdateChecker.UPDATE_AVAILABLE && resultCode == 0) {
+
+        UpdateChecker().check(this) { result ->
+            // Currently, only UpdateCheckerResult.Success is used.
+            if (result is UpdateCheckerResult.Success && result.isNewVersion) {
                 binding.startButton.setOnClickListener { downloadBuild() }
-                currentRemote = remoteVersion
+                this.currentRemote = result.latestVersion
+
                 binding.startButton.setText(R.string.action_update)
-                binding.updateAvaliable.text =
-                    "Update Available\nNewest Version available was built at $lastModified"
+                binding.updateAvaliable.text = "Update Available\nNewest Version available was built at ${result.lastModified}"
             } else {
                 binding.updateAvaliable.text = getString(R.string.no_update)
+                binding.progressIndicator.visibility = View.INVISIBLE
             }
-            binding.progressIndicator.visibility = View.INVISIBLE
+
         }
     }
 

android/chromium-updater/app/src/main/java/com/msiejak/lab/chromiumupdater/UpdateChecker.kt
@@ -1,48 +1,55 @@
 package com.msiejak.lab.chromiumupdater
 
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
 import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
-import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 
 class UpdateChecker {
 
-    companion object {
-        const val UPDATE_AVAILABLE = 1
-        const val UPDATE_NOT_AVAILABLE = 2
-        const val PARSE_ERROR = 3
-        const val NETWORK_ERROR = 4
-    }
+    fun check(context: Context, onResult: (result: UpdateCheckerResult) -> Unit) {
+        val queue = Volley.newRequestQueue(context)
 
-    fun check(c: Context, requestCode: Int, onResultCallback: (updateAvailable: Int, lastModified: String, remoteVersion: Long, resultCode: Int) -> Unit) {
-        val queue = Volley.newRequestQueue(c)
-        val s = c.getSharedPreferences("shared_prefs", AppCompatActivity.MODE_PRIVATE)
-        val currentInstalled = s.getLong("build", 0)
-        val url = "https://download-chromium.appspot.com/rev/Android?type=snapshots"
-        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null, { response ->
+        val sharedPreferences = context.getSharedPreferences("shared_prefs", MODE_PRIVATE)
+        val installedVersion = sharedPreferences.getLong("build", 0)
+
+        val request = JsonObjectRequest(VERSION_URL, null, { response ->
             try {
-                val currentRemote = response.getString("content").toLong()
-                Log.i(currentRemote.toString(), "checkForUpdate: remote")
-                Log.i(currentInstalled.toString(), "checkForUpdate: installed")
-                if (currentRemote > currentInstalled) {
-                    onResultCallback(UPDATE_AVAILABLE, response.getString("last-modified"), currentRemote, requestCode)
-                } else {
-                    onResultCallback(
-                        UPDATE_NOT_AVAILABLE, response.getString("last-modified"), currentRemote, requestCode
-                    )
-                }
+                val remoteVersion = response.getString("content").toLong()
+                Log.i(TAG, "Remote: $remoteVersion | Installed: $installedVersion")
+
+                val isNewVersion = remoteVersion > installedVersion
+                val lastModified = response.getString("last-modified")
+
+                onResult(UpdateCheckerResult.Success(isNewVersion, remoteVersion, lastModified))
             } catch (e: Exception) {
-                onResultCallback(PARSE_ERROR, "null", 0, requestCode)
+                onResult(UpdateCheckerResult.Error(ErrorType.PARSING))
             }
-        }) {
-            onResultCallback(NETWORK_ERROR, "null", 0, requestCode)
+        }, {
+            onResult(UpdateCheckerResult.Error(ErrorType.NETWORK))
+        }).apply {
+            setShouldCache(false)
+            setShouldRetryConnectionErrors(true)
         }
-        jsonObjectRequest.setShouldCache(false)
-        jsonObjectRequest.setShouldRetryConnectionErrors(true)
-        queue.add(jsonObjectRequest)
+
+        queue.add(request)
         queue.start()
     }
 
+
+    companion object {
+        private const val TAG = "UpdateChecker"
+
+        private const val VERSION_URL = "https://download-chromium.appspot.com/rev/Android?type=snapshots"
+    }
+}
+
+sealed class UpdateCheckerResult {
+    data class Success(val isNewVersion: Boolean, val latestVersion: Long, val lastModified: String): UpdateCheckerResult()
+    data class Error(val type: ErrorType): UpdateCheckerResult()
+}
+
+enum class ErrorType {
+    PARSING, NETWORK
 }

android/chromium-updater/app/src/main/java/com/msiejak/lab/chromiumupdater/service/UpdateNotificationService.kt
@@ -16,6 +16,7 @@ import androidx.work.WorkerParameters
 import com.msiejak.lab.chromiumupdater.MainActivity
 import com.msiejak.lab.chromiumupdater.R
 import com.msiejak.lab.chromiumupdater.UpdateChecker
+import com.msiejak.lab.chromiumupdater.UpdateCheckerResult
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -24,29 +25,30 @@ class UpdateNotificationService(context: Context, workerParams: WorkerParameters
     private val c = applicationContext
 
     private fun run() {
-        UpdateChecker().check(c, 1) { updateAvailable, _, _, resultCode ->
-                if (updateAvailable == UpdateChecker.UPDATE_AVAILABLE && resultCode == 1) {
-                    val intent = Intent(applicationContext, MainActivity::class.java)
-                    val pendingIntent: PendingIntent = PendingIntent.getActivity(
-                        applicationContext,
-                        0,
-                        intent,
-                        PendingIntent.FLAG_IMMUTABLE
-                    )
-                    val builder = NotificationCompat.Builder(applicationContext, "0")
-                        .setSmallIcon(R.drawable.ic_baseline_update_24)
-                        .setContentTitle(c.getString(R.string.not_title))
-                        .setContentText("An update for chromium is available")
-                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                        .setAutoCancel(true)
-                        .setContentIntent(pendingIntent)
-                    createNotificationChannel()
-                    with(NotificationManagerCompat.from(applicationContext)) {
-                        // notificationId is a unique int for each notification that you must define
-                        notify(0, builder.build())
-                    }
+        UpdateChecker().check(c) { result ->
+            // Currently, only UpdateCheckerResult.Success is used.
+            if (result is UpdateCheckerResult.Success && result.isNewVersion) {
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                val pendingIntent: PendingIntent = PendingIntent.getActivity(
+                    applicationContext,
+                    0,
+                    intent,
+                    PendingIntent.FLAG_IMMUTABLE
+                )
+                val builder = NotificationCompat.Builder(applicationContext, "0")
+                    .setSmallIcon(R.drawable.ic_baseline_update_24)
+                    .setContentTitle(c.getString(R.string.not_title))
+                    .setContentText("An update for chromium is available")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent)
+                createNotificationChannel()
+                with(NotificationManagerCompat.from(applicationContext)) {
+                    // notificationId is a unique int for each notification that you must define
+                    notify(0, builder.build())
                 }
             }
+        }
     }
 
     private fun createNotificationChannel() {

commit f18d93e6dc1c7d8010d75945f3bfc747eb23bab9

Fix issue introduced in c69cfeb

android/chromium-updater/app/src/main/java/com/msiejak/lab/chromiumupdater/MainActivity.kt
@@ -190,9 +190,9 @@ class MainActivity : AppCompatActivity() {
                 binding.updateAvaliable.text = "Update Available\nNewest Version available was built at ${result.lastModified}"
             } else {
                 binding.updateAvaliable.text = getString(R.string.no_update)
-                binding.progressIndicator.visibility = View.INVISIBLE
             }
 
+            binding.progressIndicator.visibility = View.INVISIBLE
         }
     }
 