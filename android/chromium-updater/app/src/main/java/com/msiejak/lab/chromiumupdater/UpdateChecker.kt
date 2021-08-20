c69cfebc88d20e670a45e277824f629bae1bd973
Dylan Roussel <mail@evowizz.dev>
2021-08-20 21:01:32 +0200

    Improve UpdateChecker

--- android/chromium-updater/app/src/main/java/com/msiejak/lab/chromiumupdater/UpdateChecker.kt
+++ android/chromium-updater/app/src/main/java/com/msiejak/lab/chromiumupdater/UpdateChecker.kt
@@ -1,48 +1,55 @@
 package com.msiejak.lab.chromiumupdater
 
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
 import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
-import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 
 class UpdateChecker {
 
-    companion object {
-        const val UPDATE_AVAILABLE = 1
-        const val UPDATE_NOT_AVAILABLE = 2
-        const val PARSE_ERROR = 3
-        const val NETWORK_ERROR = 4
-    }
+    fun check(context: Context, onResult: (result: UpdateCheckerResult) -> Unit) {
+        val queue = Volley.newRequestQueue(context)
 
-    fun check(c: Context, requestCode: Int, onResultCallback: (updateAvailable: Int, lastModified: String, remoteVersion: Long, resultCode: Int) -> Unit) {
-        val queue = Volley.newRequestQueue(c)
-        val s = c.getSharedPreferences("shared_prefs", AppCompatActivity.MODE_PRIVATE)
-        val currentInstalled = s.getLong("build", 0)
-        val url = "https://download-chromium.appspot.com/rev/Android?type=snapshots"
-        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null, { response ->
+        val sharedPreferences = context.getSharedPreferences("shared_prefs", MODE_PRIVATE)
+        val installedVersion = sharedPreferences.getLong("build", 0)
+
+        val request = JsonObjectRequest(VERSION_URL, null, { response ->
             try {
-                val currentRemote = response.getString("content").toLong()
-                Log.i(currentRemote.toString(), "checkForUpdate: remote")
-                Log.i(currentInstalled.toString(), "checkForUpdate: installed")
-                if (currentRemote > currentInstalled) {
-                    onResultCallback(UPDATE_AVAILABLE, response.getString("last-modified"), currentRemote, requestCode)
-                } else {
-                    onResultCallback(
-                        UPDATE_NOT_AVAILABLE, response.getString("last-modified"), currentRemote, requestCode
-                    )
-                }
+                val remoteVersion = response.getString("content").toLong()
+                Log.i(TAG, "Remote: $remoteVersion | Installed: $installedVersion")
+
+                val isNewVersion = remoteVersion > installedVersion
+                val lastModified = response.getString("last-modified")
+
+                onResult(UpdateCheckerResult.Success(isNewVersion, remoteVersion, lastModified))
             } catch (e: Exception) {
-                onResultCallback(PARSE_ERROR, "null", 0, requestCode)
+                onResult(UpdateCheckerResult.Error(ErrorType.PARSING))
             }
-        }) {
-            onResultCallback(NETWORK_ERROR, "null", 0, requestCode)
+        }, {
+            onResult(UpdateCheckerResult.Error(ErrorType.NETWORK))
+        }).apply {
+            setShouldCache(false)
+            setShouldRetryConnectionErrors(true)
         }
-        jsonObjectRequest.setShouldCache(false)
-        jsonObjectRequest.setShouldRetryConnectionErrors(true)
-        queue.add(jsonObjectRequest)
+
+        queue.add(request)
         queue.start()
     }
 
+
+    companion object {
+        private const val TAG = "UpdateChecker"
+
+        private const val VERSION_URL = "https://download-chromium.appspot.com/rev/Android?type=snapshots"
+    }
+}
+
+sealed class UpdateCheckerResult {
+    data class Success(val isNewVersion: Boolean, val latestVersion: Long, val lastModified: String): UpdateCheckerResult()
+    data class Error(val type: ErrorType): UpdateCheckerResult()
+}
+
+enum class ErrorType {
+    PARSING, NETWORK
 }
